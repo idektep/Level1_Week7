73b4ed7c40e88b75e45a3b3779a4a6cbde4ab953
idektep <idektep.coach@gmail.com>
2025-01-31 13:46:09 +0700

    Update AGV_Function_integration.ino

--- AGV_Function_integration/AGV_Function_integration.ino
+++ AGV_Function_integration/AGV_Function_integration.ino
@@ -1,20 +1,20 @@
-#define MR_IN1 12 //motor1
-#define MR_IN2 13 //motor1
-#define MR_IN3 32 //motor2
-#define MR_IN4 33 //motor2
+//-------------------------------------------------Declare variable-------------------------------------------------------//
+#define MR_IN1 12 //backright motor
+#define MR_IN2 13 //backright motor
+#define MR_IN3 33 //frontright motor
+#define MR_IN4 32 //frontright motor
+#define ML_IN3 25 //backleft motor
+#define ML_IN4 26 //backleft motor
+#define ML_IN1 14 //frontleft motor
+#define ML_IN2 27 //frontleft motor
 
-#define ML_IN1 27 //motor4
-#define ML_IN2 14 //motor4
-#define ML_IN3 25 //motor3
-#define ML_IN4 26 //motor3
+#define BR_ENA 15  //Pin speed backright motor
+#define FR_ENB 2   //Pin speed frontright motor
+#define BL_ENB 4   //Pin speed backleft motor
+#define FL_ENA 19  //Pin speed frontleft motor
 
-#define R_ENA 15  //Adjust speed  //motor1
-#define R_ENB 2   //Adjust speed //motor2
-#define L_ENA 19  //Adjust speed //motor4
-#define L_ENB 4   //Adjust speed //motor3
-
-#define L_LED 16  // ESP32 pin
-#define R_LED 17  // ESP32 pin
+#define L_LED 16  // Left LED
+#define R_LED 17  // Right LED
 
 #define TRIG 5   // Trig pin pin5
 #define ECHO 35  // Echo pin pin35
@@ -31,22 +31,23 @@ void setup() {
   pinMode(TRIG, OUTPUT);
   pinMode(ECHO, INPUT);
 
-  pinMode(L_ENA, OUTPUT);
-  pinMode(L_ENB, OUTPUT);
-  pinMode(R_ENA, OUTPUT);
-  pinMode(R_ENB, OUTPUT);
-
-  pinMode(ML_IN1, OUTPUT);
-  pinMode(ML_IN2, OUTPUT);
-  pinMode(ML_IN3, OUTPUT);
-  pinMode(ML_IN4, OUTPUT);
   pinMode(MR_IN1, OUTPUT);
   pinMode(MR_IN2, OUTPUT);
   pinMode(MR_IN3, OUTPUT);
   pinMode(MR_IN4, OUTPUT);
+  pinMode(ML_IN1, OUTPUT);
+  pinMode(ML_IN2, OUTPUT);
+  pinMode(ML_IN3, OUTPUT);
+  pinMode(ML_IN4, OUTPUT);
+  
+  pinMode(FL_ENA, OUTPUT);
+  pinMode(BL_ENB, OUTPUT);
+  pinMode(BR_ENA, OUTPUT);
+  pinMode(FR_ENB, OUTPUT);
 
   pinMode(L_LED, OUTPUT);
   pinMode(R_LED, OUTPUT);
+  delay(1000);
 }
 
 void loop() {
